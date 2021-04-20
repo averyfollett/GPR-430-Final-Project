87b6a5540bbc3f9b7f74f97ec3896827bd9c057a
Avery Follett <avery.follett@mymail.champlain.edu>
2021-04-19 23:45:35 -0400

    started setting up pressure plate class

---
+++ Source/GeneratorTest/Private/PressurePlate.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PressurePlate.h"
+
+// Sets default values
+APressurePlate::APressurePlate()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Hitbox"));
+	BoxComponent->SetupAttachment(RootComponent);
+
+	ButtonBase = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ButtonBase"));
+	ButtonBase->SetupAttachment(BoxComponent);
+
+	ButtonTop = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ButtonTop"));
+	ButtonTop->SetupAttachment(ButtonBase);
+}
+
+// Called when the game starts or when spawned
+void APressurePlate::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void APressurePlate::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/GeneratorTest/Public/PressurePlate.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "PressurePlate.generated.h"
+
+UCLASS()
+class GENERATORTEST_API APressurePlate : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* BoxComponent;
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* ButtonBase;
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* ButtonTop;
+	
+public:	
+	// Sets default values for this actor's properties
+	APressurePlate();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGuid ThisGuid;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSet<AActor*> CurrentlyPressedBy;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UMaterialInstanceDynamic* BottomMaterial;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UMaterialInstanceDynamic* TopMaterial;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsPressed = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bStaysPressed = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<AActor*> NextPowerables;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Delay = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsPowered = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector PressurePlateStart = FVector(0.0f, 0.0f, 15.0f);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector PressurePlateEnd = FVector(0.0f, 0.0f, 0.0f);
+};
